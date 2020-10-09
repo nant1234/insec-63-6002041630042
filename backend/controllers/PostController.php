3d9bddc2fbe79263d0b9f3c3ef42245a37ad3811
Nanthawan-sutthi <sutthirueangwong1@gmail.com>
2020-10-09 16:59:24 +0700

    components

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace backend\controllers;
-
 use Yii;
 use backend\models\Post;
 use backend\models\PostSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-
+// use yii\filters\AccessControl;
+use common\components\AccessControl;
 /**
  * PostController implements the CRUD actions for Post model.
  */
@@ -20,18 +18,21 @@ class PostController extends Controller
      */
     public function behaviors()
     {
-        return [ 
-            'access' => [
+        return [
+/*            'access' => [
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['index', 'create', 'delete','update','view'],
+                        'actions' => ['index','create','view','update','delete'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
                 ],
+            ],*/
+            'access' =>[
+                // 'class' => 'common\components\AccessControl'
+                'class' => AccessControl::class
             ],
-
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -40,24 +41,24 @@ class PostController extends Controller
             ],
         ];
     }
-
     /**
      * Lists all Post models.
      * @return mixed
      */
     public function actionIndex()
     {
-        if (Yii::$app->user->can('post-list'))  {
+        // if(Yii::$app->user->can('post-index')){
         $searchModel = new PostSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
         return $this->render('index', [
             'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
         ]);
-    }
+    // }
+    // else{
+    //     echo "You don't have permission to use this function.";
+    // }
 }
-
     /**
      * Displays a single Post model.
      * @param integer $id
@@ -66,12 +67,15 @@ class PostController extends Controller
      */
     public function actionView($id)
     {
-        if (Yii::$app->user->can('post-view')){
+        // if(Yii::$app->user->can('post-view')){
         return $this->render('view', [
             'model' => $this->findModel($id),
         ]);
-    }
-    }
+    // }
+    // else{
+    //     echo "You don't have permission to use this function.";
+    // }
+}
     /**
      * Creates a new Post model.
      * If creation is successful, the browser will be redirected to the 'view' page.
@@ -79,17 +83,18 @@ class PostController extends Controller
      */
     public function actionCreate()
     {
-        if (Yii::$app->user->can('post-create')){
+        // if(Yii::$app->user->can('post-create')){
         $model = new Post();
-
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         }
-
         return $this->render('create', [
             'model' => $model,
         ]);
-    }
+    // }
+    // else{
+    //     echo "You don't have permission to use this function.";
+    // }
     }
     /**
      * Updates an existing Post model.
@@ -100,18 +105,19 @@ class PostController extends Controller
      */
     public function actionUpdate($id)
     {
-        if (Yii::$app->user->can('post-update')){
+        // if(Yii::$app->user->can('post-update')){
         $model = $this->findModel($id);
-
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         }
-
         return $this->render('update', [
             'model' => $model,
         ]);
-    }
-    }
+    // }
+    // else{
+    //     echo "You don't have permission to use this function.";
+    // }
+}
     /**
      * Deletes an existing Post model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
@@ -121,12 +127,14 @@ class PostController extends Controller
      */
     public function actionDelete($id)
     {
-        if (Yii::$app->user->can('post-delete')){
+        // if(Yii::$app->user->can('post-delete')){
         $this->findModel($id)->delete();
-
         return $this->redirect(['index']);
-    }
-    }
+    // }
+    // else{
+    //     echo "You don't have permission to use this function.";
+    // }
+}
     /**
      * Finds the Post model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
@@ -139,7 +147,6 @@ class PostController extends Controller
         if (($model = Post::findOne($id)) !== null) {
             return $model;
         }
-
         throw new NotFoundHttpException('The requested page does not exist.');
     }
 }
