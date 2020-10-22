e60dd459ea99ea3cb020d679d16a49c59c8c22cf
Nanthawan-sutthi <sutthirueangwong1@gmail.com>
2020-10-22 18:26:18 +0700

    create/

--- console/migrations/m201022_104854_create_permission_of_post.php
+++ console/migrations/m201022_104854_create_permission_of_post.php
@@ -12,17 +12,57 @@ class m201022_104854_create_permission_of_post extends Migration
      */
     public function safeUp()
     {
+        $auth = Yii::$app->authManager;
+        $create = $auth->createPermission('post-create');
+        $create->description = 'Create a post';
+        $auth->add($create);
+        $index = $auth->createPermission('post-index');
+        $index->description = 'List a post';
+        $auth->add($index);
+        $delete = $auth->createPermission('post-delete');
+        $delete ->description = 'Delete a post';
+        $auth->add($delete);
+        $update = $auth->createPermission('post-update');
+        $update  ->description = 'Update a post';
+        $auth->add($update);
+        $view = $auth->createPermission('post-view');
+        $view  ->description = 'View a post';
+        $auth->add($view);
+    
 
+        
     }
-
     /**
      * {@inheritdoc}
      */
     public function safeDown()
     {
-        echo "m201022_104854_create_permission_of_post cannot be reverted.\n";
+        $auth = Yii::$app->authManager;
+		$create = $auth->getPermission('post-create');
+		if ($create){
+			$auth->remove($create);
+        }
+        $index = $auth->getPermission('post-index');
+		if ($index){
+			$auth->remove($index);
+        }
+        $delete = $auth->getPermission('post-delete');
+		if ($delete){
+			$auth->remove($delete);
+        }
+        $update = $auth->getPermission('post-update');
+		if ($update){
+			$auth->remove($update);
+        }
+        $view= $auth->getPermission('post-view');
+		if ($view){
+			$auth->remove($view);
+        }
 
-        return false;
+
+
+     
+        return true;
     }
 
     /*
